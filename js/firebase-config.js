003b7e583b42c1e40ca154203f6c16bf28c02dd0
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-03-02 10:35:29 +0530

    website in maintenance

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -35,15 +35,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
 //hehe don't be oversmart -If i get you I'll ****!
-const firebaseConfig = {
-  apiKey:            "AIzaSyAXkO4OxdHf1RtM8mLJnYNKevpPTxuKnLc",
-  authDomain:        "lostfound-e4e04.firebaseapp.com",
-  projectId:         "lostfound-e4e04",
-  storageBucket:     "lostfound-e4e04.firebasestorage.app",
-  messagingSenderId: "1082022183109",
-  appId:             "1:1082022183109:web:80f6393d186abaeafab0ac",
-  measurementId:     "G-GLRBNNQSRJ"
-};
 
 
 const app      = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
